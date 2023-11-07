5213480ec224821b75c1168f28f82c78f802a2a5
Nicolas BITAILLOU <nicolas.bitaillou@etu.univ-nantes.fr>
2023-11-07 17:43:55 +0100

    Add Content in Main2

--- Main2.py
+++ Main2.py
@@ -30,6 +30,73 @@ class TransitionList:
             for state in states_to:
                 print(f"  Transition to -> {state.get_name()} (Label: {state.get_label()})")
 
+class TransitionSystem():
+    def __init__(self, S, Action, Transition, I, Prop):
+        self.S = S
+        self.Action = Action
+        self.Transition = Transition
+        self.I = I
+        self.Prop= Prop
+
+def verif_invariant(ST):
+    # Set of accessible states
+    R = set()
+    # Stack of states
+    U = []
+    # Boolean value, initialized to True
+    verif = True
+
+    while verif and ST:
+        s = ST.I  # On choisit arbitrairement un état initial
+        if s not in R:
+            visiter(s)
+
+    if verif:
+        return "OUI"
+    else:
+        return "NON", self.U
+    
+def visiter(self, s):
+        self.U.append(s)
+        self.R.add(s)
+        print("dans visiter")
+        while self.U and self.b:
+            s_prime = self.U[-1]
+            self.U.insert(0,s_prime)
+            print("dans le while")
+            if s_prime in self.R:
+                self.U.pop()
+                content_pile = self.U 
+                print(content_pile)
+                self.b = self.b and self.check_property(s_prime)
+                print("if")
+            else:
+                print("else")
+                s_double_prime = None  # Choose a state from Post(s') that is not in R
+                # Assuming Post is a function that needs to be defined
+                for state in self.Post(s_prime):
+                    if state not in self.R:
+                        s_double_prime = state
+                        print("break")
+                        break
+
+                if s_double_prime:
+                    self.U.append(s_double_prime)
+                    self.R.add(s_double_prime)
+                    print("if double prime")
+                else:
+                    print("else pop")
+                    self.U.pop()
+
+def check_property(self, s_prime):
+        print("check_property")
+        return True
+
+def Post(self, s_prime):
+    print("post")
+    # Placeholder for post function logic
+    # Needs to be implemented
+    return []
 
 
 
@@ -45,6 +112,8 @@ def main():
     State8 = State(8,{"p1","c2"})
 
     States = {State1,State2,State3,State4,State5,State6,State7,State8}
+
+    Action =["epsilon"]
 
     # State1.get_name()
     # State1.get_label()
@@ -83,5 +152,15 @@ def main():
 
     print("Tableau des transitions")
     transitionList.display_transitions()
+
+    Prop ={"n1","n2","p1","p2","c1","c2"}
+    I ={State1}
+
+
+    Phi = Or(Not(Unary({"c1"})),Not(Unary({"c2"})))
+
+    ST = TransitionSystem(8,Action,transitionList,I,Phi)
+
+    verif_invariant(ST)
 
 main()
